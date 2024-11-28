b552e6182cf9f865b2d265322d020b3466283c7f
w191635 <sahil.srivastava@worldline.com>
2024-11-28 15:18:03 +0530

    Cleaning

--- src/main/java/com/etms/worldline/Repository/ArchivedTrainingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.etms.worldline.Repository;
-
-import com.etms.worldline.model.ArchivedTrainings;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArchivedTrainingsRepository extends JpaRepository<ArchivedTrainings, Long> {
-}
